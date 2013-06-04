1099b61ec66efb02a97e3655e2e8ef1051321ab5
David Cuddeback <david.cuddeback@gmail.com>
2013-06-03 17:59:04 -0700

    fixes test failures for Clojure 1.3

--- test/pail_cascalog/core_test.clj
+++ test/pail_cascalog/core_test.clj
@@ -1,6 +1,6 @@
 (ns pail-cascalog.core-test
-  (:require [pail-cascalog.core :refer :all]
-            [midje.sweet :refer :all])
+  (:require [pail-cascalog.core :as pail])
+  (:use midje.sweet)
   (:import (com.backtype.hadoop.pail PailStructure PailSpec PailPathLister)
            (com.backtype.cascading.tap PailTap$PailTapOptions)
            (org.apache.hadoop.fs Path)))
@@ -17,26 +17,26 @@
 
     (facts "without options"
       (fact "creates a PailTapOptions with default properties"
-        (tap-options spec) => (instance-of PailTap$PailTapOptions)
-        (.spec      (tap-options spec)) => spec
-        (.fieldName (tap-options spec)) => "bytes"
-        (.attrs     (tap-options spec)) => nil?
-        (.lister    (tap-options spec)) => nil?))
+        (pail/tap-options spec) => (instance-of PailTap$PailTapOptions)
+        (.spec      (pail/tap-options spec)) => spec
+        (.fieldName (pail/tap-options spec)) => "bytes"
+        (.attrs     (pail/tap-options spec)) => nil?
+        (.lister    (pail/tap-options spec)) => nil?))
 
 
     (facts "with options"
       (fact "creates a PailTapOptions with custom field name"
-        (.fieldName (tap-options spec :field-name "foo")) => "foo")
+        (.fieldName (pail/tap-options spec :field-name "foo")) => "foo")
 
       (fact "creates a PailTapOptions with custom attributes"
-        (seq (.attrs (tap-options spec :attributes [["foo" "bar"] ["baz"]]))) => (just [["foo" "bar"] ["baz"]]))
+        (seq (.attrs (pail/tap-options spec :attributes [["foo" "bar"] ["baz"]]))) => (just [["foo" "bar"] ["baz"]]))
 
       (fact "treats a list of strings as a single attributes"
-        (seq (.attrs (tap-options spec :attributes ["foo" "bar" "baz"]))) => (just [["foo" "bar" "baz"]]))
+        (seq (.attrs (pail/tap-options spec :attributes ["foo" "bar" "baz"]))) => (just [["foo" "bar" "baz"]]))
 
       (fact "creates a PailTapOptions with custom PailPathLister"
         (let [lister (proxy [PailPathLister] [])]
-          (.lister (tap-options spec :lister lister)) => lister)))))
+          (.lister (pail/tap-options spec :lister lister)) => lister)))))
 
 
 (facts "tap"
@@ -45,10 +45,10 @@
         options-bar (PailTap$PailTapOptions. spec "bar" nil nil)]
 
     (fact "creates a PailTap with the given path"
-      (.getPath (tap options-foo "foo/bar")) => (Path. "foo/bar")
-      (.getPath (tap options-foo "baz")) => (Path. "baz"))
+      (.getPath (pail/tap options-foo "foo/bar")) => (Path. "foo/bar")
+      (.getPath (pail/tap options-foo "baz")) => (Path. "baz"))
 
     (fact "creates a PailTap with the given PailTapOptions"
-      (.. (tap options-foo "foo/bar") getScheme getSpec) => spec
-      (.. (tap options-foo "foo/bar") getScheme getSourceFields (get 1)) => "foo"
-      (.. (tap options-bar "foo/bar") getScheme getSourceFields (get 1)) => "bar")))
+      (.. (pail/tap options-foo "foo/bar") getScheme getSpec) => spec
+      (.. (pail/tap options-foo "foo/bar") getScheme getSourceFields (get 1)) => "foo"
+      (.. (pail/tap options-bar "foo/bar") getScheme getSourceFields (get 1)) => "bar")))
